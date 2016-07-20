5afc119e3c1712b4915ac4fa177939911bc8bcd8
Daniel Couch <couchds@email.wofford.edu>
2016-07-20 23:11:18 +0100

    ...

--- environment.cpp
+++ environment.cpp
@@ -7,11 +7,6 @@
     *      Implementation of environment class             * 
     ********************************************************/ 
 
-#include <iostream>
-#include <string.h>
-#include <SDL2/SDL.h>
-#include <SDL2_image/SDL_image.h>
-#include "window_management.h"
 #include "environment.h"
 
 
---
+++ environment.h
@@ -0,0 +1,6 @@
+
+#include <iostream>
+#include <string.h>
+#include <SDL2/SDL.h>
+#include <SDL2_image/SDL_image.h>
+#include "window_management.h"
